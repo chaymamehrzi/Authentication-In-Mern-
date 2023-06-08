381f3ff767d56b5001bfc5ffe7fd86a569293431
chaymamehrzi <chayma.mehrzi@esprit.tn>
2023-06-08 19:40:50 +0100

    last changes

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dbconf = require('./dbconfig/db.json');
+const crypto = require('crypto');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+
+//midddlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
+
+mongoose.set('strictQuery', true);
+mongoose.connect(dbconf.mongo.uri);
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbconf.mongo.uri);
+})
+
+const port = process.env.PORT || 8000; 
+app.listen(port,()=>console.log(`Listening on port $(port)...`));
+
+const privateKey = crypto.randomBytes(32).toString('hex');
+
+//console.log(privateKey);
+
+
+module.exports = app; 
